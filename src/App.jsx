ca619fa4caeb406e26b19ada94fde0650773bda6
Pablo J Lebed <contact@pablolebed.dev>
2025-07-16 19:26:46 +0200

    centering fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import Header from "../components/Header"
-import Main from "../components/Main"
-import Footer from "../components/Footer"
-import "./index.css"
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Footer from "../components/Footer";
 
 export default function App() {
   return (
-    <>
+    <div className="container">
+
       <Header />
       <Main />
-      <Footer /> 
-    </>
-  )
+      <Footer />
+
+    </div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./index.css"
 
 const root = createRoot(document.getElementById("root"));
-root.render(
-  <div className="container">
-    <App />
-  </div>
-);
+root.render(<App />);
